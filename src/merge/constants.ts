eadaacb517c0ea0f6f05cc6870ad3227588aa746
dragonhunt02 <jackhunt43@libero.it>
2024-12-18 21:24:49 +0100

    Update constants.ts

--- src/merge/constants.ts
+++ src/merge/constants.ts
@@ -2,6 +2,7 @@
 export enum Inputs {
   Name = 'name',
   Pattern = 'pattern',
+  Regex = 'regex',
   SeparateDirectories = 'separate-directories',
   RetentionDays = 'retention-days',
   CompressionLevel = 'compression-level',
